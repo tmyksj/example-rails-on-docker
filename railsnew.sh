fb1648afdf987e207d97e098782317b4f6b8e5ee
tmyksj <33417830+tmyksj@users.noreply.github.com>
2018-07-11 12:06:29 +0900

    Update example

--- railsnew.sh
+++ railsnew.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run --entrypoint /bin/bash --rm -it -v `pwd`/app:/var/www/app -v `pwd`/data/app:/var/www/data/app erod-rails -c " \
+docker run --entrypoint /bin/bash --rm -it -v `pwd`/app:/var/www/app -v `pwd`/data/app:/var/www/data/app erod-app -c " \
 bundle install \
 && bundle exec rails new . --database=mysql --force --skip-bundle --skip-git \
 && rm config/database.yml \
